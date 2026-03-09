9a6e6aa8346e5d76cdc007ceb3ae26a277f77880
Arindam Ghosh <arindamghosh@Arindams-MacBook-Air.local>
2026-03-09 15:21:09 +0100

    Note App with Clean architecture and MVI

---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,23 @@
+package com.example.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.noteapp.ui.theme.BabyBlue
+import com.example.noteapp.ui.theme.LightGreen
+import com.example.noteapp.ui.theme.RedOrange
+import com.example.noteapp.ui.theme.RedPink
+import com.example.noteapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content : String,
+    val color : Int,
+    val timeStamp : Long,
+    @PrimaryKey val id : Int?=null
+){
+    companion object{
+        val noteColors = listOf(RedOrange, LightGreen,RedPink, BabyBlue, Violet)
+    }
+}
+class InvalidNoteException(message: String): Exception(message)
---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,8 @@
+package com.example.noteapp.feature_note.domain.use_case
+
+data class NoteUseCases(
+    val getListOfNotes: GetListOfNoteUseCase,
+    val deleteNote: DeleteNoteUse,
+    val addNote: AddNoteUseCase,
+    val getNote: GetNoteUseCase
+)
